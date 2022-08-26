8955791882ec0968499ea1482de9814455749ba6
dankturtle <abrowncownow@gmail.com>
2022-08-25 23:56:58 -0700

    still bugged

--- script.js
+++ script.js
@@ -6,54 +6,56 @@ var gameScore = {
 }
 choices = ["Rock", "Paper", "Scissors"]
 var playAgain = true;
-while (playAgain){
-    var playerChoice = window.prompt("Choose R, P, or S", "R, P, S");
-    if (!(playerChoice === "R") || (playerChoice === "P") || (playerChoice === "S")){
-        window.alert("You entered an invalid choice. Try again!"); }
-    else if ((playerChoice === "R") || (playerChoice === "P") || (playerChoice === "S")){
+while (!playAgain===false){
+    //establish gameloop function
+    function gameLoop(){
+    var playerChoice = window.prompt("Choose R, P, or S");
+    if ((playerChoice === "R") || (playerChoice === "P") || (playerChoice === "S")){
         //translate choice into player value
         var playerValue;
         if (playerChoice === "R"){
-            playerValue = "Rock";
+            playerValue = choices[0];
         } else if (playerChoice === "P"){
-            playerValue = "Paper";
+            playerValue = choices[1];
         } else if (playerChoice ==="S"){
-            playerValue = "Scissors";}
+            playerValue = choices[2];}
         
         //Declare playerValue for debugging
         window.alert("You chose " + playerValue + "!"); 
         //computer's move
-        var computerChoice = Math.floor(Math.random()*3)        
-        window.alert("The computer chose " + choices[computerChoice] + "!")
-        var computerValue = choices[computerChoice]
+        var computerChoice = Math.floor(Math.random()*3);
+        window.alert("The computer chose " + choices[computerChoice] + "!");
+        var computerValue = choices[computerChoice];
 
         //determine outcome
         if (playerValue === computerValue){
-            windows.alert("It's a Tie!")
+            windows.alert("It's a tie!");
             gameScore.ties++;
         }
+        //win conditions
         else if(
             (playerValue === "Rock" && computerValue === "Scissors") ||
             (playerValue === "Paper" && computerValue === "Rock") ||
             (playerValue === "Scissors" && computerValue === "Paper")){
                 window.alert("You won!");
-                gameScore.wins++;
-        } else{
+                gameScore.wins++;} 
+        //lose
+        else{
             window.alert("The computer won.");
             gameScore.losses++;
         }
-
         //display stats
         window.alert("Stats:" + 
         "\nWins: " + gameScore.wins +
         "\nLosses: " + gameScore.losses +
-        "\nTies: " + gameScore.ties)
-
-        //Check if the player wants to play again
-        function checkPlayAgain(){
-        playAgain = window.confirm("Play again?");}
-        checkPlayAgain();
-    }else{}
-
-   
+        "\nTies: " + gameScore.ties);
+    }else{window.alert("You entered an invalid choice. Try again!");
+        gameLoop();}
+    }
+    gameLoop();
+    //Check if the player wants to play again
+    function checkPlayAgain(){
+    playAgain = window.confirm("Play again?");}
+    checkPlayAgain();
+ 
 }
